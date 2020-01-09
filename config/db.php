3ca5c4d02c9623aea3d049c64ea48dafe81e07c9
DESKTOP-KQT7VQT\tem <784637485@qq.com>
2020-01-09 14:29:32 +0800

    tjp --更新配置文件

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=47.112.117.135;dbname=customized',
+    'dsn' => 'mysql:host=47.112.117.135;dbname=customized_db',
     'username' => 'www',
     'password' => 'mqQ7sJ7CSLZztTaY',
     'charset' => 'utf8mb4',
