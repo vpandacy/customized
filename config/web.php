ac48d966cdd4187ad3b45cd549f1c980dfc7b606
DESKTOP-KQT7VQT\tem <784637485@qq.com>
2020-01-12 09:24:42 +0800

    tjp --注释gii

--- config/web.php
+++ config/web.php
@@ -128,19 +128,19 @@ $config = [
 
 if (true) {
 //     configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-//     uncomment the following to add your IP if you are not connecting from localhost.
-    'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-//         uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+//    $config['bootstrap'][] = 'debug';
+//    $config['modules']['debug'] = [
+//        'class' => 'yii\debug\Module',
+////     uncomment the following to add your IP if you are not connecting from localhost.
+//    'allowedIPs' => ['127.0.0.1', '::1'],
+//    ];
+//
+//    $config['bootstrap'][] = 'gii';
+//    $config['modules']['gii'] = [
+//        'class' => 'yii\gii\Module',
+////         uncomment the following to add your IP if you are not connecting from localhost.
+//        'allowedIPs' => ['127.0.0.1', '::1'],
+//    ];
 }
 
 return $config;
