5b5f7224c9a6c34a6f6520ab4c9b654564e9ae6b
vpanda <897513405@qq.com>
2020-01-12 08:38:08 +0800

    删除vendor

--- vendor/codeception/base/tests/coverage/RemoteServerWithHtmlCept.php
+++
@@ -1,6 +0,0 @@
-<?php
-$I = new CoverGuy($scenario);
-$I->wantTo('try generate remote codecoverage xml report');
-$I->amInPath('tests/data/sandbox');
-$I->executeCommand('run remote_server --coverage-html remote_server');
-$I->seeFileFound('index.php', 'tests/_output/remote_server');
--- vendor/codeception/base/tests/coverage/RemoteWithHtmlCept.php
+++
@@ -1,7 +0,0 @@
-<?php
-$I = new CoverGuy($scenario);
-$I->wantTo('try generate remote codecoverage xml report');
-$I->amInPath('tests/data/sandbox');
-$I->executeCommand('run remote --coverage-html');
-$I->seeFileFound('index.php', 'tests/_output/coverage');
-$I->seeCoverageStatsNotEmpty();
